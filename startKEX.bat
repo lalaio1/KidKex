0afc9f436d00ac9701c6236054f5272acc99fc78
lalaio1 <122741162+lalaio1@users.noreply.github.com>
2024-05-03 22:54:12 -0300

    Create startKEX.bat

---
+++ startKEX.bat
@@ -0,0 +1,53 @@
+@echo off
+echo Encerrando processos pesados que não afetam o Windows...
+taskkill /F /IM chrome.exe
+taskkill /F /IM firefox.exe
+taskkill /F /IM edge.exe
+taskkill /F /IM discord.exe
+taskkill /F /IM slack.exe
+taskkill /F /IM teams.exe
+taskkill /F /IM spotify.exe
+taskkill /F /IM vlc.exe
+taskkill /F /IM notepad.exe
+taskkill /F /IM calculator.exe
+taskkill /F /IM paint.exe
+taskkill /F /IM wordpad.exe
+taskkill /F /IM excel.exe
+taskkill /F /IM powerpnt.exe
+taskkill /F /IM outlook.exe
+taskkill /F /IM acroRd32.exe
+taskkill /F /IM chrome.exe
+taskkill /F /IM firefox.exe
+taskkill /F /IM edge.exe
+taskkill /F /IM opera.exe
+taskkill /F /IM brave.exe
+taskkill /F /IM iexplore.exe
+taskkill /F /IM discord.exe
+taskkill /F /IM slack.exe
+taskkill /F /IM teams.exe
+taskkill /F /IM skype.exe
+taskkill /F /IM zoom.exe
+taskkill /F /IM telegram.exe
+taskkill /F /IM whatsapp.exe
+taskkill /F /IM spotify.exe
+taskkill /F /IM vlc.exe
+taskkill /F /IM winamp.exe
+taskkill /F /IM itunes.exe
+taskkill /F /IM foobar2000.exe
+taskkill /F /IM audacity.exe
+taskkill /F /IM notepad.exe
+taskkill /F /IM wordpad.exe
+taskkill /F /IM excel.exe
+taskkill /F /IM powerpnt.exe
+taskkill /F /IM outlook.exe
+taskkill /F /IM onenote.exe
+taskkill /F /IM acroRd32.exe
+taskkill /F /IM calculator.exe
+taskkill /F /IM paint.exe
+taskkill /F /IM snippingtool.exe
+taskkill /F /IM taskmgr.exe
+taskkill /F /IM regedit.exe
+taskkill /F /IM msconfig.exe
+echo Processos encerrados com sucesso.
+wsl kex
+exit 
